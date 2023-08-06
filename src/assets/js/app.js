d9df81a38f256d7488f2d19789090864f468c625
nonshinam <nonshinam@gmail.com>
2023-08-06 15:47:01 +0430

    ..

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -72,3 +72,47 @@ $(".list-tab .shape1").each(function(){
 })
 
 
+
+//tab list page useraccount
+
+$(document).ready(function() {
+    // تابعی برای تغییر تب و محتوای مربوطه
+    function changeTab(tab) {
+        // حذف کلاس active از تب ها و محتواها
+        $('.tab').removeClass('active');
+        $('.content').removeClass('active');
+
+        // اضافه کردن کلاس active به تب و محتوای مربوطه
+        $(tab).addClass('active');
+        $('.content[data-tab="' + $(tab).data('tab') + '"]').addClass('active');
+    }
+
+    // تغییر تب با کلیک بر روی آن
+    $('.tab').click(function() {
+        changeTab(this);
+    });
+
+    // تغییر تب با استفاده از کلیدهای تب
+    $(document).keydown(function(e) {
+        if (e.which === 9) { // کلید Tab فشرده شده است
+            e.preventDefault(); // جلوگیری از عملکرد پیش فرض کلید Tab
+
+            var currentTab = $('.tab.active'); // تب فعلی
+            var nextTab;
+
+            if (e.shiftKey) { // کلید Shift همزمان با کلید Tab فشرده شده است
+                nextTab = currentTab.prev('.tab'); // تب قبلی
+            } else {
+                nextTab = currentTab.next('.tab'); // تب بعدی
+            }
+
+            if (nextTab.length === 0) { // در صورتی که تب بعدی وجود نداشته باشد، به اولین تب برگرد
+                nextTab = $('.tab:first');
+            }
+
+            changeTab(nextTab);
+        }
+    });
+});
+
+
